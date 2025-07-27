0feb1136dc6c2011154638e8b3348f298d998772
Uncle-Roach <justconnor13@gmail.com>
2025-07-27 12:54:10 -0700

    Update Boxes.lua

--- Boxes.lua
+++ Boxes.lua
@@ -2,37 +2,36 @@ local plr = game.Players.LocalPlayer
 local box = Instance.new('BillboardGui')
 local frame = Instance.new('Frame')
 local Teams = game:GetService('Teams'):GetTeams()
-local runservice = game:GetService('RunService')
 
 box.Name = 'Name'
 frame.Name = 'Name'
 
-box.Size = UDim2.new(4.5, 0,6.5, 0)
+box.Size = UDim2.new(1.2, 0,1.2, 0)
 box.Parent = nil
 box.AlwaysOnTop = true
 
 frame.Size = UDim2.new(1, 0,1, 0)
 frame.Position = UDim2.new(0,0,0,0)
 frame.BackgroundColor3 = Color3.fromRGB(170,0,0)
-frame.BackgroundTransparency = 0.75
+frame.BackgroundTransparency = 0.2
 frame.Parent = box
 
 local function setup(player)
 	if player == plr then return end
-	local boxESP = box:Clone()
+	local HeadESP = box:Clone()
 	local char = player.Character or player.CharacterAdded:Wait()
-	
-	local humRP = char:WaitForChild('HumanoidRootPart')
+    
+	local Head = char:WaitForChild('Head')
 	if #Teams > 0 then
 		if player.Team == plr.Team then return end
-	end
-  if humRP:FindFirstChild('Name') then return end
-	if humRP then boxESP.Parent = humRP else return end
+    end
+    if Head:FindFirstChild('Name') then return end
+	if Head then HeadESP.Parent = Head else return end
 end
 
 local function addPlr(player)
 	setup(player)
-	
+
 	player.CharacterAdded:Connect(function()
 		setup(player)
 	end)
@@ -50,7 +49,4 @@ end
 
 game.Players.PlayerAdded:Connect(function(plr)
 	addPlr(plr)
-  runservice.Heartbeat:Connect(function()
-    addPlr(plr)
-  end)
 end)
